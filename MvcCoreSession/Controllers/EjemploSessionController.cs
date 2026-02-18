a946b2c29cde496331a5f3f673a7bfe842addbf9
Jorgerdzz <asilgalan083@gmail.com>
2026-02-18 09:57:21 +0100

    Ejemplo Session simple

---
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreSession.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        public IActionResult SessionSimple(string accion)
+        {
+            if(accion != null)
+            {
+                if(accion.ToLower() == "almacenar")
+                {
+                    //GUARDAMOS DATOS EN SESSION
+                    HttpContext.Session.SetString("nombre", "Jorge");
+                    HttpContext.Session.SetString("hora", DateTime.Now.ToLongTimeString());
+                    ViewData["MENSAJE"] = "Datos almacenados en Session";
+                }else if(accion.ToLower() == "mostrar")
+                {
+                    //RECUPERAMOS LOS DATOS DE SESSION
+                    ViewData["NOMBRE"] = HttpContext.Session.GetString("nombre");
+                    ViewData["HORA"] = HttpContext.Session.GetString("hora");
+
+                }
+            }
+            return View();
+        }
+    }
+}
