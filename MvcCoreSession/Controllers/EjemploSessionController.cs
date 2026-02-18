9a14d6881b82f0c7e66062e0ba673c592157b3e6
Jorgerdzz <asilgalan083@gmail.com>
2026-02-18 11:41:00 +0100

    Lista mascotas finalizada

--- MvcCoreSession/Controllers/EjemploSessionController.cs
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -56,5 +56,32 @@ namespace MvcCoreSession.Controllers
             }
             return View();
         }
+
+        public IActionResult SessionMascotaCollectionBytes(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    List<Mascota> mascotasList = new List<Mascota>
+                    {
+                        new Mascota{Nombre= "Nala", Raza="Leona", Edad=12},
+                        new Mascota{Nombre= "Sebastian", Raza="Cangrejo", Edad=17},
+                        new Mascota{Nombre= "Rafiki", Raza="Brujo", Edad=33},
+                        new Mascota{Nombre= "Olaf", Raza="Muñeco", Edad=22},
+                    };
+                    byte[] data = HelperBinarySession.ObjectToByte(mascotasList);
+                    HttpContext.Session.Set("MASCOTAS", data);
+                    ViewData["MENSAJE"] = "Coleccion guardada";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    byte[] data = HttpContext.Session.Get("MASCOTAS");
+                    List<Mascota> mascotas = (List<Mascota>)HelperBinarySession.ByteToObject(data);
+                    return View(mascotas);
+                }
+            }
+            return View();
+        }
     }
 }
