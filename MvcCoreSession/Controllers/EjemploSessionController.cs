22e9b17975a3706ebfaa9e343846f0bceb2a98a9
Jorgerdzz <asilgalan083@gmail.com>
2026-02-18 11:18:44 +0100

    Serialization realizada

--- MvcCoreSession/Controllers/EjemploSessionController.cs
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreSession.Helpers;
+using MvcCoreSession.Models;
 
 namespace MvcCoreSession.Controllers
 {
     public class EjemploSessionController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public IActionResult SessionSimple(string accion)
         {
             if(accion != null)
@@ -20,6 +27,31 @@ namespace MvcCoreSession.Controllers
                     ViewData["NOMBRE"] = HttpContext.Session.GetString("nombre");
                     ViewData["HORA"] = HttpContext.Session.GetString("hora");
 
+                }
+            }
+            return View();
+        }
+
+        public IActionResult SessionMascotaBytes(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    Mascota mascota = new Mascota();
+                    mascota.Nombre = "Garfield";
+                    mascota.Raza = "gato";
+                    mascota.Edad = 7;
+                    //Para almacenar la mascota en Session, debemos convertirlo a byte[]
+                    byte[] data = HelperBinarySession.ObjectToByte(mascota);
+                    HttpContext.Session.Set("MASCOTA",data);
+                    ViewData["MENSAJE"] = "Mascota almacenada en Session";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    byte[] data = HttpContext.Session.Get("MASCOTA");
+                    Mascota mascota = (Mascota)HelperBinarySession.ByteToObject(data);
+                    ViewData["MASCOTA"] = mascota;
                 }
             }
             return View();
